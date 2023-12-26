b2016a84562fe8a412fba8189264e9da04e9bc7a
Saar Zilbershtein <saar.zilbershtein@post.runi.ac.il>
2023-12-26 19:42:28 +0200

    Final

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -7,7 +7,7 @@ public class DamkaBoard {
         int n = Integer.parseInt(args[0]);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(n % 2 == 0 ? "* " : " *");
+                System.out.print(i % 2 == 0 ? "* " : " *");
             }
             System.out.println();
         }
